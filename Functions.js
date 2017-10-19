e5e455b825eb0c5ef1ab6c450f5a5433feaf0294
Alfredo Munoz <AlfredoM@Alfredos-MacBook-Pro.local>
2017-10-19 17:35:05 -0400

    Added word guessing functions to Functions.js

--- Functions.js
+++ Functions.js
@@ -3,4 +3,24 @@ var letters = require("./Letters.js");
 
 // Functions that will be exported to 'Hangman.js'
 
-// 
+var Word = function(solution){
+	this.solution = solution;
+	this.characterCount = solution.length;
+	this.reveal = "";
+	this.letters = [];
+	// 
+	this.revealCharacter = function() {
+		this.reveal = "";
+		for (var i = 0; i < this.letters.length; i++) {
+			this.reveal += this.letters[i].reveal + " ";
+		}
+	};
+	// Once user guesses a letter the Character will be revealed
+	this.letterGuess = function(guess) {
+		for (var i = 0; i < this.letters.length; i++) {
+			this.letters[i].check(guess);
+		}
+		this.revealCharacter();
+	};
+
+}
